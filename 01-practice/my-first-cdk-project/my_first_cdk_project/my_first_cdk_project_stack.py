edf65b98f6bcadee87276f5087e986b9d0c5fbb0
siddharth201983 <sharma.siddharth2009@gmail.com>
2023-12-17 16:03:36 +0530

    day 1

--- 01-practice/my-first-cdk-project/my_first_cdk_project/my_first_cdk_project_stack.py
+++ 01-practice/my-first-cdk-project/my_first_cdk_project/my_first_cdk_project_stack.py
@@ -5,7 +5,7 @@ from aws_cdk import (
 )
 from constructs import Construct
 
-class 01PracticeStack(Stack):
+class MyFirstCdkProjectStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
@@ -14,6 +14,6 @@ class 01PracticeStack(Stack):
 
         # example resource
         # queue = sqs.Queue(
-        #     self, "01PracticeQueue",
+        #     self, "MyFirstCdkProjectQueue",
         #     visibility_timeout=Duration.seconds(300),
         # )
--- 01-practice/my-first-cdk-project/tests/unit/test_my_first_cdk_project_stack.py
+++ 01-practice/my-first-cdk-project/tests/unit/test_my_first_cdk_project_stack.py
@@ -1,13 +1,13 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from 01_practice.01_practice_stack import 01PracticeStack
+from my_first_cdk_project.my_first_cdk_project_stack import MyFirstCdkProjectStack
 
 # example tests. To run these tests, uncomment this file along with the example
-# resource in 01_practice/01_practice_stack.py
+# resource in my_first_cdk_project/my_first_cdk_project_stack.py
 def test_sqs_queue_created():
     app = core.App()
-    stack = 01PracticeStack(app, "01-practice")
+    stack = MyFirstCdkProjectStack(app, "my-first-cdk-project")
     template = assertions.Template.from_stack(stack)
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
