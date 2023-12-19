ca1278dd6d25f0fa0ea4e011c09dbac368e411fb
siddharth201983 <sharma.siddharth2009@gmail.com>
2023-12-19 10:59:28 +0530

    day 1 complete

--- 01-practice/my-first-cdk-project/my_first_cdk_project/my_first_cdk_project_stack.py
+++ 01-practice/my-first-cdk-project/my_first_cdk_project/my_first_cdk_project_stack.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env node --no-warnings
-
+import aws_cdk as core
 from aws_cdk import (
     Stack,
-    aws_s3 as _s3
+    aws_s3 as _s3,
+    aws_iam as _iam
 )
 from constructs import Construct
 
@@ -18,6 +19,30 @@ class MyFirstCdkProjectStack(Stack):
             self,
             "myBucketId",
             bucket_name="myfirstcdkproject-sid-2024",
-            versioned=True,
+            versioned=False,
             encryption=_s3.BucketEncryption.S3_MANAGED,
+            block_public_access=_s3.BlockPublicAccess.BLOCK_ALL,
+        )
+
+        mybucket = _s3.Bucket(
+            self,
+            "myBucketId1"
+        )
+
+        snstopicname = "abcxyz1234"
+        if not core.Token.is_unresolved(snstopicname) and len(snstopicname) > 10:
+            raise ValueError("Max value length can be 10 char only")
+        print(mybucket.bucket_name)
+
+        _iam.Group(
+            self,
+            "gid"
+        )
+
+        output_1 = core.CfnOutput(
+            self,
+            "myBucketOutput1",
+            value=mybucket.bucket_arn,
+            description="My first CDK Bucket",
+            export_name="myBucketOutput1"
         )
