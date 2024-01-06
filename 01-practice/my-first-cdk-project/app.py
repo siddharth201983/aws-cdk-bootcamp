1e58c072d1f9cc6492a9b219c2dc3b2f6a419a7b
siddharth201983 <sharma.siddharth2009@gmail.com>
2024-01-06 21:07:55 +0530

    day5

--- 01-practice/my-first-cdk-project/app.py
+++ 01-practice/my-first-cdk-project/app.py
@@ -8,9 +8,13 @@ import aws_cdk as cdk
 
 # from resource_stacks.custom_ec2_with_ebs_prov_iops import CustomEc2PiopsStack
 
-from app_stacks.vpc_stack import VpcStack
+# from app_stacks.vpc_stack import VpcStack
 
-from app_stacks.web_server_stack import WebServerStack
+# from app_stacks.web_server_stack import WebServerStack
+
+# from resource_stacks.custom_parameters_stack import CustomParametersSecretsStack
+
+from resource_stacks.custom_iam_users_groups import CustomIamUsersGroupsStack
 
 app = cdk.App()
 
@@ -24,9 +28,13 @@ env = cdk.Environment(account=app.node.try_get_context('envs')['prod']['account'
 
 # CustomEc2PiopsStack(app, "my-ec2-stack-with-prov-iops", env=env)
 
-vpc_stack = VpcStack(app, "vpc-stack", env=env)
+# vpc_stack = VpcStack(app, "vpc-stack", env=env)
 
-WebServerStack(app, "webserver-stack-with-asg-elb", env=env, vpc=vpc_stack.vpc)
+# WebServerStack(app, "webserver-stack-with-asg-elb", env=env, vpc=vpc_stack.vpc)
+
+# CustomParametersSecretsStack(app, "my-ssm-params-stack", env=env)
+
+CustomIamUsersGroupsStack(app, "my-iam-users-groups-stack", env=env)
 
 cdk.Tags.of(app).add("Email", app.node.try_get_context('envs')['prod']['email'])
 
---
+++ 01-practice/my-first-cdk-project/resource_stacks/custom_iam_users_groups.py
@@ -0,0 +1,72 @@
+import aws_cdk as cdk
+from constructs import Construct
+import json
+
+
+class CustomIamUsersGroupsStack(cdk.Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        
+        # AWS Secrets & SSM parameters
+        param1 = cdk.aws_ssm.StringParameter(
+            self,
+            "parameter1",
+            description="Load Testing Configuration",
+            parameter_name="NoOfConcurrentUsers",
+            string_value="100",
+            tier=cdk.aws_ssm.ParameterTier.STANDARD,
+        )
+        
+        param2 = cdk.aws_ssm.StringParameter(
+            self,
+            "parameter2",
+            description="Load Testing Configuration",
+            parameter_name="/locust/configs/NoOfConcurrentUsers",
+            string_value="100",
+            tier=cdk.aws_ssm.ParameterTier.STANDARD,
+        )
+        
+        param3 = cdk.aws_ssm.StringParameter(
+            self,
+            "parameter3",
+            description="Load Testing Configuration",
+            parameter_name="/locust/configs/DurationInSec",
+            string_value="300",
+            tier=cdk.aws_ssm.ParameterTier.STANDARD,
+        )
+        
+        secret1 = cdk.aws_secretsmanager.Secret(
+            self,
+            "secret1",
+            description="Customer DB password",
+            secret_name="cust_db_pass"
+        )
+        
+        templated_secret = cdk.aws_secretsmanager.Secret(
+            self,
+            "templated_secret",
+            description="Customer DB password in the form of templated secret",
+            secret_name="cust_secret_attrobute",
+            generate_secret_string=cdk.aws_secretsmanager.SecretStringGenerator(
+                secret_string_template=json.dumps(
+                    {"username": "user1"}
+                ),
+            generate_string_key="secret_password"
+            )
+        )
+        
+        cdk.CfnOutput(
+            self,
+            "param1Output",
+            description="NoOfConcurrentUsers",
+            value=f"{param1.string_value}",
+        )
+        
+        cdk.CfnOutput(
+            self,
+            "secret1Output",
+            description="secret1",
+            value=f"{secret1.secret_value}",
+        )
+        
